22004a4cc96b6fa44641677efc2c86743f595e1d
medimed66 <mehdi.elbouari@epfl.ch>
2022-12-02 15:46:39 +0100

    thymio controls

--- Motion_Control/thymio_control.py
+++ Motion_Control/thymio_control.py
@@ -1,18 +1,88 @@
+from tdmclient import ClientAsync, aw
+import numpy as np
+
+'''thymio dimensions'''
+r=22 #mm
+l=48 #mm
+
+'''controller constants to tune accordingly'''
+kp=25   #>0
+ka=50  # > kp
+kb=-0.0001 #<0
+
+'''speed limits and sensors thresholds to tune accordingly'''
+v_max=1000
+v_min=100
+thres_arrived=10
 
 
-def rotate(direction, speed, time):
-    if direction == 'left':
-        print('Rotation gauche')
-    elif direction == 'right':
-        print('Rotation droite')
+def thym_motors(right, left):
+    """
+    Create the speed structure to control the Thymio's motors (low level)
+    :param right: speed of the right motor
+    :param left: speed of the left motor
+    :return: structure needed by the Thymio to access peripherals
+    """
+    return {"motor.left.target": [left], "motor.right.target": [right]}
 
 
-def advance(speed, time):
-    print('Avance le robot')
+def set_motor_speed(sr, sl, node):
+    """
+    Set the speed of our thymio
+    :param sr: speed of the right motor
+    :param sl: speed of the left motor
+    :param node: link to our robot
+    """
+    aw(node.set_variables(thym_motors(sr, sl)))
 
 
-def detect_obstacle():
-    return False
+def stop_motors(node):
+    """
+    Stop motor motion
+    :param node: link to our robot
+    :return:
+    """
+    set_motor_speed(0, 0, node)
 
-def stop_thymio():
-    print( 'robot stoped' )
+
+def get_motors_speed(node, client):
+    """
+    Function to get access to current motors' speed
+    :param node:
+    :param node, client: link to our robot
+    :return: structure containing motors' speed ([speed_right, speed_left])
+    """
+    aw(node.wait_for_variables({"motor.left.speed", "motor.right.speed"}))
+    aw(client.sleep(0.05))
+    return [node.v.motor.right.speed, node.v.motor.left.speed]
+
+
+def get_prox_sensors(node, client):
+    """
+    Function to get access to current motors' speed
+    :param node:
+    :param node, client: link to our robot
+    :return: structure containing proximity sensors' values ([prox1,..., prox8])
+    """
+    aw(node.wait_for_variables({"prox.horizontal"}))
+    aw(client.sleep(0.05))
+    return node.v.prox.horizontal
+
+def astolfi(pos, teta, target, node):
+    state=0 #this functions is called recursivly untill state=1 i.e. the thymio has arrived
+    delta_pos=np.substract(target,pos)
+    rho=np.linal.norm(delta_pos)
+    alpha=-teta + np.arctan2(delta_pos[1],delta_pos[0])
+    beta=-teta-alpha
+    omega=ka*alpha+kb*beta
+    if rho>thres_arrived:
+        v=kp*rho
+        if v>v_max: v=v_max
+        if v<v_min: v=v_min
+    else:
+        v=0
+        state=1
+    left_speed=int(v-l*omega)
+    right_speed=int(v+l*omega)
+    set_motor_speed(right_speed, left_speed, node)
+    return state
